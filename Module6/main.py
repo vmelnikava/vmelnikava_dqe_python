de58248e52f1a74437f94e5b95fe5e56a4f05792
vmelnikava <77779051+vmelnikava@users.noreply.github.com>
2021-03-05 19:13:42 +0300

    Adding xml support

--- Module6/main.py
+++ Module6/main.py
@@ -3,21 +3,27 @@ sys.path.append('D:\\Python_DQE\\Module5')
 sys.path.append('D:\\Python_DQE\\Module4')
 sys.path.append('D:\\Python_DQE\\Module7')
 sys.path.append('D:\\Python_DQE\\Module8')
+sys.path.append('D:\\Python_DQE\\Module9')
 import publishing_input as pi
 import process_batch as pb
 import functions as f
 import generate_csv
 import process_json as pj
+import process_xml as px
 
 
 def select_mode():
-    mode = input(
-        f"""Enter which mode you would like to run publishing:\n1 - Input Mode\n2 - Batch Mode\n3 - JSON Mode\n\nYour Input:\n""")
+    mode = input(f"""Enter which mode you would like to run publishing:
+    1 - Input Mode
+    2 - Batch Mode
+    3 - JSON Mode
+    4 - XML Mode
+    0 - Exit\nYour Input:\n""")
     return mode
 
 
 def normalize_file():
-    print('Log: applying case normalization to newsfeed')
+    print('\nLog: applying case normalization to newsfeed')
     with open(rf"{pi.default_output_path}\\" + "newsfeed_input.txt", 'r') as file:
         norm_data = f.normalize(file.read())
     norm_data = norm_data.replace('Privatead', 'PrivateAd')
@@ -34,8 +40,12 @@ def main():
         pb.main()
     elif mode == '3':
         pj.main()
+    elif mode == '4':
+        px.main()
+    elif mode == '0':
+        pi.good_bye()
     else:
-        print('ERROR: incorrect input (1,2 or 3 is expected)')
+        print('ERROR: incorrect input (1,2,3 or 4 is expected)')
         exit(0)
     normalize_file()
     generate_csv.main(pi.default_output_path, pi.output_file_name)
