46f8b766442fcf7081b082b7761c1571be0f8274
vmelnikava <77779051+vmelnikava@users.noreply.github.com>
2021-03-05 19:15:02 +0300

    Moved general functions to main

--- Module8/process_json.py
+++ Module8/process_json.py
@@ -4,24 +4,16 @@ sys.path.append('D:\\Python_DQE\\Module5')
 import publishing_input as pi
 import json
 
-default_json_path = 'D:\\Python_DQE\\Module6\\files'
-input_json_name = 'json_input.json'
 
+input_json_name = 'json_input.json'
 dict_json_format = {'News': ('city', 'text'),
                     'PrivateAd': ('text', 'expiration date'),
                     'Horoscope': ('period', 'zodiac sign', 'prediction')}
 
 
-def get_path():
-    input_path = input('Enter a path to file or leave it empty for default path:\n')
-    if not input_path:
-        input_path = default_json_path
-    return input_path
-
-
 def get_json_data(p_path):
+    print(f'\nLog: looking for file in {p_path}')
     try:
-        print(f'\nLog: looking for file in {p_path}')
         with open(rf"{p_path}\\" + f"{input_json_name}", "r+"):
             print('Log: file was found successfully!\n')
             return json.load(open(rf"{p_path}\\" + f"{input_json_name}"))
@@ -57,25 +49,16 @@ def process_json(p_json_data):
     validate_object_params(p_json_data)
     for item in p_json_data:
         list_args = tuple(item.values())
-        print(f'Log: start creating object with parameters {list_args}')
-        new_obj = eval('pi.' + 'Create' + list_args[0]).get_input_json(*list_args)
+        print(f'Log (JSON) start creating object with parameters {list_args}')
+        new_obj = eval('pi.' + 'Create' + list_args[0]).get_input_json_xml(*list_args)
         new_obj.publish(new_obj.prepare_output())
 
 
-def delete_file(p_path, p_file_name):
-    print('Log: start deleting input file..')
-    try:
-        os.remove(rf"{p_path}\\" + f"{p_file_name}")
-    except IOError:
-        print('EXCEPTION: failed to delete the file')
-    print('Log: input file has been deleted')
-
-
 def main():
-    json_path = get_path()
+    json_path = pi.get_path()
     json_data = get_json_data(json_path)
     process_json(json_data)
-    delete_file(json_path, input_json_name)
+    pi.delete_file(json_path, input_json_name)
 
 
 if __name__ == "__main__":
