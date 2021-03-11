d192f9ad290a02d5a1b9fa5a693b040ef979ef34
vmelnikava <77779051+vmelnikava@users.noreply.github.com>
2021-03-11 20:10:04 +0300

    Update classBatch.py

--- Module6/classBatch.py
+++ Module6/classBatch.py
@@ -9,7 +9,6 @@
 
 import re
 import sys
-
 sys.path.append('D:\\Python_DQE\\Module5')
 import publishing_input as pi
 
@@ -18,12 +17,14 @@ class processBatch:
     def __init__(self, p_input_data):
         self.batch_data = p_input_data
 
-    def validate_object_type(self, param_list):
+    @staticmethod
+    def validate_object_type(param_list):
         if param_list[0] not in ('News', 'PrivateAd', 'Horoscope'):
             print('EXCEPTION: failed to parse, invalid object type')
             exit(1)
 
-    def validate_parameters(self, param_list):
+    @staticmethod
+    def validate_parameters(param_list):
         if (
                 (param_list[0] == 'News' and len(param_list) != 3) or
                 (param_list[0] == 'PrivateAd' and len(param_list) != 3) or
@@ -31,8 +32,9 @@ class processBatch:
             print('EXCEPTION: failed to parse, invalid number of parameters given')
             exit(1)
 
-    def publish_batch_record(self, record, list):
-        new_obj = eval('pi.' + 'Create' + record[0]).get_input_batch(*list)
+    @staticmethod
+    def publish_batch_record(record, param_list):
+        new_obj = eval('pi.' + 'Create' + record[0]).get_input_batch(*param_list)
         new_obj.publish(new_obj.prepare_output())
         param_list_db = new_obj.prepare_output_db()
         pi.publish_to_db(record[0], f"{param_list_db}")
