a5c87f229c4587be1de46d32ab88c2100fbfdf6a
vmelnikava <77779051+vmelnikava@users.noreply.github.com>
2021-03-12 16:47:00 +0300

    Update classJson.py

--- Module8/classJson.py
+++ Module8/classJson.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('D:\\Python_DQE\\Module5')
-import publishing_input as pi
+import classNews, classPrivateAd, classHoroscope
 
 dict_json_format = {'News': ('city', 'text'),
                     'PrivateAd': ('text', 'expiration date'),
@@ -19,7 +19,7 @@ class processJson:
                 exit(1)
 
     def validate_object_params(self):
-        print('Log: validating object parameters...\n')
+        print('Log: validating object parameters...')
         for i in range(len(self.json_data)):
             current_keys = self.json_data[i].keys()
             list_of_expected_params = dict_json_format.get(self.json_data[i].get('type'))
@@ -31,13 +31,17 @@ class processJson:
                     print('EXCEPTION: failed to parse, invalid parameter names given for:\n', self.json_data[i])
                     exit(1)
 
+    @staticmethod
+    def publish_json_record(list_args):
+        print(f'\nLog: (JSON) start processing row {list_args}')
+        new_obj = eval('class' + list_args[0] + '.create' + list_args[0]).get_input_json_xml(*list_args)
+        new_obj.publish(new_obj.prepare_output())
+        param_list_db = new_obj.prepare_output_db()
+        new_obj.publish_to_db(f"{param_list_db}")
+
     def parse_json(self):
         self.validate_object_type()
         self.validate_object_params()
         for item in self.json_data:
             list_args = tuple(item.values())
-            print(f'Log: (JSON) start creating object with parameters {list_args}')
-            new_obj = eval('pi.' + 'Create' + list_args[0]).get_input_json_xml(*list_args)
-            new_obj.publish(new_obj.prepare_output())
-            param_list_db = new_obj.prepare_output_db()
-            pi.publish_to_db(list_args[0], f"{param_list_db}")
+            self.publish_json_record(list_args)
